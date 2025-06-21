631bef9af6dfeb1fbf58fd6d6ab82b466fe0f745
asadbek07 <futureprotrader1007@gmail.com>
2025-06-22 02:31:08 +0500

    Add ignoreDuringBuilds to nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 console.log("Next config loaded ✅");
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["via.placeholder.com", "firebasestorage.googleapis.com"],
   },
